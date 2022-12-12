58cebbd8dc6d18a2020a7d62fef6443572e712dc
Edwin Tang <edwintang10@gmail.com>
2022-12-12 09:25:21 +0800

    chore(directory): segregate user-facing and admin pages.

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,9 @@ export default function RootLayout({
       <head />
       <body>
         <Header />
-        {children}
+        <div className="p-5">
+          {children}
+        </div>
       </body>
     </html>
   )
